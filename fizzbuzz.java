5b8c0a3945c746f077bcecffa375ec1d9a9fc63b
Danny Lantigua <dannygrullon7@gmail.com>
2018-06-22 14:44:38 -0400

    Changes

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,3 +1,7 @@
+/*
+  From 1 to 100, print "Fizz" when the number is divisible by 3, print "Buzz" when the number is divisible by 5, print "FizzBuzz" when the number is divisible by 3 and 5
+*/
+
 public Class FizzBuzz{
     public static void main (String args[]){
         for(int a = 1; a <= 100; a++){
